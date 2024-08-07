729b11c780f4b70eb44850a1a0d50af2e57a35c0
yashdarji30 <123467783+yashdarji30@users.noreply.github.com>
2024-08-07 15:22:04 +0530

    user controller

---
+++ backend/models/notification.model.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { mongo } from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    from:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    to:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    type:{
+        type:String,
+        required:true,
+        enum: ["follow","like"] //add to the comment
+    },
+    read:{
+        type: Boolean,
+        default: false,
+    },
+    
+},{timestamps:true});
+
+const Notification = mongoose.model("Notification",notificationSchema);
+
+export default Notification;
--- backend/server.js
+++ backend/server.js
@@ -2,22 +2,22 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import { v2 as cloudinary } from "cloudinary";
+import { v2 as cloudinary } from "cloudinary";
 
 import authRoutes from "./routes/auth.routes.js";
-// import userRoutes from "./routes/user.route.js";
+import userRoutes from "./routes/user.routes.js";
 // import postRoutes from "./routes/post.route.js";
-// import notificationRoutes from "./routes/notification.route.js";
+import notificationRoutes from "./routes/notification.route.js";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 
 dotenv.config();
 
-// cloudinary.config({
-// 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-// 	api_key: process.env.CLOUDINARY_API_KEY,
-// 	api_secret: process.env.CLOUDINARY_API_SECRET,
-// });
+cloudinary.config({
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -30,9 +30,9 @@ app.use(express.urlencoded({ extended: true })); // to parse form data(urlencode
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
-// app.use("/api/users", userRoutes);
+app.use("/api/users", userRoutes);
 // app.use("/api/posts", postRoutes);
-// app.use("/api/notifications", notificationRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 // if (process.env.NODE_ENV === "production") {
 // 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
