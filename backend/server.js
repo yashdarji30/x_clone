b009985e840bca08d161dcd67a73a7a38a3f1f35
yashdarji30 <123467783+yashdarji30@users.noreply.github.com>
2024-10-22 11:37:20 +0530

    reup

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(express.json({limit: "5mb"})); // to parse req.body
-// limit shouldn't be too high to prevent DOS
+// limit shouldn't be too high to prevent DDOS
 app.use(express.urlencoded({ extended: true })); // to parse form data(urlencoded)
 
 app.use(cookieParser());
